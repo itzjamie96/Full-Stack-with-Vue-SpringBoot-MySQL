4b084bff888b100154f920b4579bd430d976c39d
kboysm <lishal12@naver.com>
2019-12-24 16:58:09 +0900

    [Add 로그인 완료 back & front]

--- everymung01_back/src/main/java/org/salem/domain/Mapper/SitterMapper.java
+++ everymung01_back/src/main/java/org/salem/domain/Mapper/SitterMapper.java
@@ -3,9 +3,10 @@ package org.salem.domain.Mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.salem.domain.vo.SitterVO;
-import org.salem.domain.vo.UsersVO;
 
 @Mapper
 public interface SitterMapper {
@@ -14,6 +15,14 @@ public interface SitterMapper {
 	
 	public SitterVO showSitterDetail(int sitterNo); //시터 상세보기
 	
-	@Select("select * from sitter where sitterEmail=#{email}") //시터 로긴 & 새로고침 관련
-	public SitterVO showUserDetailLogin(String email);
+	@Select("select * from sitter where sitterEmail=#{sitterEmail}") //시터 로긴 & 새로고침 관련
+	public SitterVO showUserDetailLogin(@Param("sitterEmail") String sitterEmail);
+	
+	@Select("INSERT INTO sitter (sittingNo,sitterEmail,sitterName,sitterPw,sitterPhone,sitterAddress,sitterImg1,sitterImg2,sitterImg3,sitterImg4,sitterImg5,possibleDay,sitterGender,sitterAge,profile,petProfile,identityCheck,qualificationCheck,approvalStatus,approvalDate)" + 
+			" VALUES" + 
+			" (#{sittingNo},#{sitterEmail},#{sitterName},#{sitterPw},#{sitterPhone},#{sitterAddress},#{sitterImg1},#{sitterImg2},#{sitterImg3},#{sitterImg4},#{sitterImg5},#{possibleDay},1,33,'자기소개','펫소개',1,1,1,sysdate())")
+	public Integer addSitter(SitterVO vo);
+	
+	@Update("update sitter set identityCheck=#{identityCheck},qualificationCheck=#{qualificationCheck} where sitterEmail=#{sitterEmail}")
+	public void sitterCheck(SitterVO vo);
 }
